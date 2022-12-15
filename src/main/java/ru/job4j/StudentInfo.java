4c109169251ca98f8fed28b721887a48bb053d97
Eduard <ebiryulin@gmail.com>
2022-12-15 16:59:41 +0300

    4.1. Разбор первой программы. [#159033 #370867]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Eduard Biryulin");
+        System.out.println("28.02.1985");
+
+    }
+}
