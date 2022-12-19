d94a2b8937a53ca021a339d321a6324c262ea190
Eduard <ebiryulin@gmail.com>
2022-12-19 14:51:42 +0300

    7. Расстояние между точками в системе координат [#188 #372592]

---
+++ src/test/java/ru/job4j/CountTest.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class CountTest {
+    public static void main(String[] args) {
+        int i = 105;
+        char c = (char) i;
+        //int result = one + two;
+        System.out.println(c);
+        }
+    }
