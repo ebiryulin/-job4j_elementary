e03ad8f5c1769a82b3b390cdf6ec71d4ec65c9a6
Eduard <ebiryulin@gmail.com>
2022-12-29 16:37:53 +0300

    6.4.0. Слово начинается с ... [#41585 #375905]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,10 +3,10 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         for (char j = 0; j < pref.length; j++) {
-                    if (word[j] != pref[j]) {
-                        return false;
-                    }
-                }
+            if (word[j] != pref[j]) {
+                return false;
+            }
+        }
         return true;
     }
 }
