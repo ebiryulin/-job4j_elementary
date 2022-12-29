81f7343f6fe994f4e189c9ca9074ceda667d9a19
Eduard <ebiryulin@gmail.com>
2022-12-29 11:33:18 +0300

    6.2.3. Перевернуть массив. [#226790 #375704]

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -2,14 +2,13 @@ package ru.job4j.array;
 
 public class Check {
     public static boolean mono(boolean[] data) {
-        boolean result = true;
+        //boolean result = true;
         for (int index = 0; index < data.length - 1; index++) {
-            if (!result) {
+            if (data[index] != data[data.length - index - 2]) {
                 return false;
             }
-
         }
-        return result;
+        return true;
     }
 }
 
