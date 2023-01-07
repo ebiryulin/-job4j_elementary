461cdc339c8be7a9d6a1ede0bd457c5bb996be81
Eduard <ebiryulin@gmail.com>
2023-01-07 17:23:10 +0300

    6.9. String - это массив [#504884]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -1,58 +1,32 @@
 package ru.job4j.array;
 
-import javax.swing.*;
-
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
-        for (int i = 0; i < name.length(); i++) {
-            int code = name.codePointAt(i);
-            //if (code != isSpecialSymbol(code) && code != isUpperLatinLetter(code)
-            //&& isLowerLatinLetter(code) && isDigit(code) && isUpperCase(code)
-            //&& isEmpty()) {
-            //return false;
+        boolean rsl = name.length() != 0
+                && !Character.isDigit(name.charAt(0))
+                && isNotUpperLatinLetter(name.charAt(0));
+        if (rsl) {
+            for (int i = 1; i < name.length(); i++) {
+                int code = name.codePointAt(i);
+                if (isNotSpecialSymbol(code) && isNotUpperLatinLetter(code)
+                        && isNotLowerLatinLetter(code) && !Character.isDigit(code)) {
+                    rsl = false;
+                    break;
+                }
+            }
         }
-        return false;
+        return rsl;
     }
 
-    public static boolean isSpecialSymbol(int code) {
-        if (code != 36 || code != 95) {
-            return false;
-        }
-        return true;
+     private static boolean isNotSpecialSymbol(int code) {
+        return code != 36 && code != 95;
     }
 
-    public static boolean isUpperLatinLetter(int code) {
-        if (code < 65 || code > 90) {
-            return false;
-        }
-        return true;
+    private static boolean isNotUpperLatinLetter(int code) {
+        return code < 65 || code > 90;
     }
 
-    public static boolean isLowerLatinLetter(int code) {
-        if (code < 97 || code > 122) {
-            return false;
-        }
-        return true;
-    }
-
-    public static boolean isDigit(int code) {
-        if (code < 49  || code > 57) {
-            return false;
-        }
-        return true;
-    }
-
-    public static boolean isUpperCase(int code) {
-        if (code >= 65  || code <= 90) {
-            return false;
-        }
-        return true;
-    }
-
-    public boolean isEmpty() {
-        if (isEmpty()) {
-            return true;
-        }
-        return false;
+    private static boolean isNotLowerLatinLetter(int code) {
+        return code < 97 || code > 122;
     }
 }
